415c4d071693cc236ae4333e73d1d2988ac769e1
User . <e41210003@student.polije.ac.id>
2024-11-12 11:22:58 +0700

    Feat: Halus Kasar

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import matplotlib.image as mpimg
 import serial
 import time
 import mahotas
+import os
+from skimage.feature import graycomatrix, graycoprops
 
 try:
     arduino = serial.Serial('COM3', 9600, timeout=1)
@@ -127,27 +129,20 @@ class WebcamApp:
             print("No image to save!")
 
 
-    def process_image(self, path, contrast_factor=1.0):
+    def process_image(self, path):
         if os.path.exists(path):
-            # Read the image from the provided path using OpenCV
+            # Membaca gambar dari path yang diberikan
             image = cv2.imread(path)
 
             if image is None:
                 print("Image not found at path:", path)
                 return
 
-            # Save the original image
+            # Simpan gambar asli
             cv2.imwrite("Original_Image.png", image)
             print("Image successfully read and saved as 'Original_Image.png'.")
 
-            # Adjust contrast
-            adjusted_image = self.adjust_contrast(image, contrast_factor)
-
-            # Save the contrast-adjusted image
-            cv2.imwrite("Adjusted_Contrast_Image.png", adjusted_image)
-            print("Contrast-adjusted image saved as 'Adjusted_Contrast_Image.png'.")
-
-            # Convert image to Grayscale
+            # Konversi gambar ke Grayscale
             img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
             # Segmentasi Citra Menggunakan Thresholding
@@ -159,181 +154,62 @@ class WebcamApp:
             if contours:
                 largest_contour = max(contours, key=cv2.contourArea)
                 x, y, w, h = cv2.boundingRect(largest_contour)
-                pixel_panjang = w
-                pixel_lebar = h
-                # Hitung dimensi
-                panjang = 0.0895 * pixel_panjang - 3.1795
 
-                # Tentukan kualitas daun
-                kualitas = self.determine_leaf_quality(panjang)
-
-                # Deteksi kerusakan dan minyak 
+                # Deteksi kerusakan dan minyak
                 cropped_image = image[y:y+h, x:x+w]
 
                 # Buat masker untuk citra yang dipotong
                 mask = np.zeros((h, w), dtype=np.uint8)
-
-                # Sesuaikan koordinat kontur ke citra yang dipotong
                 adjusted_contour = largest_contour - [x, y]
-
-                # Isi area kontur pada masker
                 cv2.drawContours(mask, [adjusted_contour], -1, 255, thickness=cv2.FILLED)
-                # Segmentasikan objek dengan masker
                 segmented_image = cv2.bitwise_and(cropped_image, cropped_image, mask=mask)
-                cv2.imwrite('2 Image Segmented.png', segmented_image)
-
-                # Temukan kontur pada gambar masker
-                contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-                # Buat gambar kosong dengan ukuran yang sama dengan segmented_image
-                inner_mask = np.zeros_like(mask)
-
-                # Loop melalui setiap kontur yang ditemukan
-                for contour in contours:
-                    # Shrink kontur ke dalam dengan jarak 50 piksel menggunakan offset
-                    offset_distance = 40
-
-                    # Buat offset dengan -50 piksel ke dalam menggunakan `cv2.drawContours`
-                    # Offset dilakukan dengan menggeser titik-titik kontur ke dalam menggunakan erosi
-                    eroded_mask = np.zeros_like(mask)
-                    cv2.drawContours(eroded_mask, [contour], -1, 255, thickness=cv2.FILLED)
-
-                    # Menggunakan erosi untuk membuat kontur baru di dalam kontur asli
-                    kernel = np.ones((offset_distance, offset_distance), np.uint8)
-                    inner_contour_mask = cv2.erode(eroded_mask, kernel, iterations=1)
-
-                    # Temukan kontur baru pada inner_contour_mask
-                    inner_contours, _ = cv2.findContours(inner_contour_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-                    # Gambar kontur baru ke inner_mask
-                    cv2.drawContours(inner_mask, inner_contours, -1, 255, thickness=cv2.FILLED)
-
-                # Terapkan inner_mask sebagai masker ke gambar asli yang di-cropped
-                result_with_inner_contour = cv2.bitwise_and(cropped_image, cropped_image, mask=inner_mask)
-
-                # Simpan hasil akhir dengan kontur di dalam kontur
-                cv2.imwrite('3 result_with_inner_contour.png', result_with_inner_contour)
+                cv2.imwrite('2_Image_Segmented.png', segmented_image)
 
                 if len(segmented_image.shape) == 3:
                     # Konversi citra berwarna (3 channel) menjadi grayscale
-                    segmented_image_gray = cv2.cvtColor(segmented_image, cv2.COLOR_RGBA2GRAY)
-                    cv2.imwrite('4 segmented_image_gray.png', segmented_image_gray)
-                    # Menggunakan Gaussian Blur sebelum deteksi tepi
+                    segmented_image_gray = cv2.cvtColor(segmented_image, cv2.COLOR_BGR2GRAY)
+                    cv2.imwrite('4_segmented_image_gray.png', segmented_image_gray)
 
-                    inner_segmented_gray = cv2.cvtColor(result_with_inner_contour, cv2.COLOR_RGBA2GRAY)
-                    cv2.imwrite('5 Inner_Segmented_Gray.png', inner_segmented_gray)
+                    # Perhitungan GLCM
+                    glcm = graycomatrix(segmented_image_gray, distances=[1], angles=[0], symmetric=True, normed=True)
 
-                                # Calculate GLCM features and classify texture
-                    glcm_features = self.calculate_glcm(segmented_image_gray)
-                    texture = self.classify_texture(glcm_features)
-                    print(f"Leaf texture: {texture}")
-                    
-                    # Tentukan rentang warna hitam (minyak)
-                    lower_black = np.array([1], dtype=np.uint8)
-                    upper_black = np.array([32], dtype=np.uint8)
+                    # Normalisasi dan konversi GLCM ke format yang bisa disimpan sebagai gambar
+                    glcm_image = (glcm[:, :, 0, 0] * 255).astype(np.uint8)
+                    cv2.imwrite("GLCM_Matrix.png", glcm_image)
+                    print("GLCM matrix saved as 'GLCM_Matrix.png'.")
 
-                    # Buat mask untuk warna hitam
-                    black_mask = cv2.inRange(inner_segmented_gray, lower_black, upper_black)
-                    
-                    # Deteksi tepi dengan Canny untuk menemukan tulang daun
-                    edges = cv2.Canny(inner_segmented_gray, 50, 150)
-                    cv2.imwrite('6 edges.png', edges)
+                    # Ekstraksi fitur GLCM
+                    contrast = graycoprops(glcm, 'contrast')[0, 0]
+                    dissimilarity = graycoprops(glcm, 'dissimilarity')[0, 0]
+                    homogeneity = graycoprops(glcm, 'homogeneity')[0, 0]
+                    energy = graycoprops(glcm, 'energy')[0, 0]
+                    correlation = graycoprops(glcm, 'correlation')[0, 0]
 
-                    # Dilatasi untuk mempertebal tepi tulang daun
-                    kernel = np.ones((1, 3), np.uint8)
-                    dilated_edges = cv2.dilate(edges, kernel, iterations=1)
-                    cv2.imwrite('7 dilated_edges.png', dilated_edges)
+                        # Menentukan kategori tekstur berdasarkan threshold
+                    if contrast <= 115:
+                        texture = "Halus"
+                    elif 115 < contrast <= 120:
+                        texture = "Sedang"
+                    else:
+                        texture = "Kasar"
 
-                    # Buat mask tulang daun
-                    leaf_veins_mask = cv2.bitwise_not(dilated_edges)
-                    cv2.imwrite('8 leaf_veins_mask.png', leaf_veins_mask)
-
-                    # Terapkan leaf_veins_mask pada black_mask agar tulang daun tidak ikut terdeteksi sebagai minyak
-                    filtered_black_mask = cv2.bitwise_and(black_mask, black_mask, mask=leaf_veins_mask)
-                    black_pixels_filtered = cv2.countNonZero(filtered_black_mask)
-
-                    # Ganti piksel hitam (minyak) dengan warna kuning pada gambar BGR
-                    filtered_black_mask_bgr = cv2.cvtColor(filtered_black_mask, cv2.COLOR_GRAY2BGR)
-                    filtered_black_mask_bgr[np.where((filtered_black_mask_bgr == [255, 255, 255]).all(axis=2))] = [0, 255, 255]
-                    cv2.imwrite('9 filtered_blackMaskBgr.png', filtered_black_mask_bgr)
-
-                    print("Jumlah piksel hitam setelah filter tulang daun:", black_pixels_filtered)
-
-                    PanjangDaun = max(panjang, 0)
-                    # Mengirim data ke Arduino
-                    # Kategori warna berdasarkan average_hue dan average_value
-
-                    # Format string yang ingin dikirim
-                    grading = f"{kualitas}" 
-                    self.send_hue_and_color_category(kualitas, grading)                    
+                    # Cetak hasil GLCM
+                    print("GLCM Features:")
+                    print(f"Contrast: {contrast}")
+                    print(f"Dissimilarity: {dissimilarity}")
+                    print(f"Homogeneity: {homogeneity}")
+                    print(f"Energy: {energy}")
+                    print(f"Correlation: {correlation}")
                     self.label_dimensions.config(
-                        text=f"Grade:\n {kualitas} \nPanjang Asli: {panjang:.1f}\nPanjang: {pixel_panjang}\nLebar: {pixel_lebar}\nPixel: {black_pixels_filtered}\nEntropy: {glcm_features[4]:.2f}\nTekstur: {texture}"
+                        text=f"Texture: {texture}\ncontrast: {contrast:.3f}\nDissimilarity: {dissimilarity:.3f}\nHomogenity: {homogeneity:.3f}\nEnergy: {energy:.3f}\nCorrelation: {correlation:.3f}"
                     )
 
-                else: 
+                else:
                     print("Segmented Not Found")
             else:
                 print("Contour Not Found")
         else:
             print("Image path not found.")
-
-    
-    def adjust_contrast(self, image, factor):
-        # Mengonversi gambar ke format float32 agar tidak terjadi overflow
-        adjusted = cv2.convertScaleAbs(image, alpha=factor, beta=0)
-        return adjusted
-
-    def calculate_glcm(self, image):
-        # Calculate GLCM features using Haralick method and take the mean across different directions
-        glcm = mahotas.features.haralick(image).mean(axis=0)
-        return glcm
-
-    def classify_texture(self, glcm_features):
-        # Print all GLCM feature values for debugging
-        print("GLCM Feature Values:", glcm_features)
-        
-        # Classification thresholds based on Entropy (glcm_features[4])
-        if glcm_features[4] <= 0.45:
-            texture = "Halus"  # Smooth texture
-        elif 0.45 < glcm_features[4] <= 0.475:
-            texture = "Sedang"  # Moderate texture
-        else:
-            texture = "Kasar"  # Rough texture
-
-        # Print the selected feature (Entropy) and the classification result
-        print(f"Entropy: {glcm_features[4]}, Classified as: {texture}")
-        return texture
-
-
-        # Print the selected feature (Entropy) and the classification result
-        print(f"Entropy: {glcm_features[4]}, Classified as: {texture}")
-        return texture
-
-
-    def send_hue_and_color_category(self, oil, color_category):
-        data = f"{oil},{color_category}\n"
-        self.arduino.write(data.encode())  # Kirim data ke Arduino
-        print(f"Data dikirim: {data.strip()}")
-
-    def determine_leaf_quality(self, panjang):
-        print(f"Panjang daun: {panjang}")
-        if panjang < 5:
-            return "-"
-        elif panjang > 45.1:
-            print("Masuk kategori Super")
-            return "Super"
-        elif 40.1 <= panjang <= 45.1:
-            print("Masuk kategori Lente 1")
-            return "Lente 1"
-        elif 35.1 <= panjang < 40.1:
-            print("Masuk kategori Lente 2")
-            return "Lente 2"
-        elif 29.8 <= panjang < 35.1:
-            print("Masuk kategori Lente 3")
-            return "Lente 3"
-        else:
-            print("Masuk kategori Filler")
-            return "Filler"
 
 root = tk.Tk()
 app = WebcamApp(root)
